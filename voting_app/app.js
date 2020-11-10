ebf40759a0a4ab895d3bd3da0592d863271e7c82
Tawhid <gem@tawhid.fios-router.home>
2020-11-09 20:25:51 -0500

    first product listed

--- voting_app/app.js
+++ voting_app/app.js
@@ -2,13 +2,40 @@ class ProductList extends React.Component {
   render() {
     return(
       <div className='ui unstackable items'>
-        Hello friend, Im a basic component
+        <Product />
       </div>
     );
   }
 }
 
+class Product extends React.Component{
+  render(){
+    return (
+      <div className="item">
+        <div className='image'>
+          <img src='images/products/image-aqua.png' />
+        </div>
+        <div className='middle aligned content'>
+            <div className='description'>
+              <a>Fort Knight</a>
+              <p>Authentic renaissance actors, delivered in just two week\s.</p>
+            </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+            className='ui avatar image'
+            src='images/avatars/daniel.jpg' />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
 ReactDOM.render(
   <ProductList />,
   document.getElementById('content')
 );
+
